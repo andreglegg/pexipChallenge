1ec0a0a92a132d8acc49bc129b68e1ef1f06c18a
andreglegg <andreglegg@me.com>
2020-05-17 23:46:05 +0200

    clean up, setup css modules, and installed reactstrap and related packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = (tab: React.SetStateAction<string>) => {
+        if(activeTab !== tab) setActiveTab(tab);
+    }
+
+    return (
+        <div>
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { toggle('1'); }}
+                    >
+                        Tab1
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { toggle('2'); }}
+                    >
+                        Tab2
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        <Col sm="12">
+                            <h4>Tab 1 Contents</h4>
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="12">
+                            tab 2
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
+        </div>
+    );
 }
 
 export default App;
