6ccce1858bc891f1da92ed2b03b7e4e380caa406
andreglegg <andreglegg@me.com>
2020-06-01 12:04:09 +0200

    added first link preview

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useSelector} from 'react-redux';
 import {Anchorme} from 'react-anchorme';
-import classNames from "classnames";
-import EditBox from "./EditBox";
+import { ReactTinyLink } from 'react-tiny-link';
+import classNames from 'classnames';
+
+import { validURL, formatUrl } from '../../utils/UrlHelper';
+import EditBox from './EditBox';
+import EditInput from '../EditInput/EditInput';
+import {State} from '../../types/State';
+import {User} from '../../types/User';
+import {Message} from '../../types/Message';
 
 import styles from './MessageItem.module.scss';
-import EditInput from "../EditInput/EditInput";
-import {State} from "../../types/State";
-import {User} from "../../types/User";
-import {Message} from "../../types/Message";
 
 interface IProps {
     user: User;
@@ -25,16 +28,28 @@ const MessageItem = (props: IProps) => {
     const created = new Date(message.createdAt).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
     const updated = new Date(message.updatedAt).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
 
-    const renderMessage = message.isDeleted ? 'DELETED' : (<Anchorme target="_blank" rel="noreferrer noopener">{message.message}</Anchorme>);
+    const renderMessage = message.isDeleted ? 'DELETED' : (<Anchorme target='_blank' rel='noreferrer noopener'>{message.message}</Anchorme>);
+    const firstLink = message.message.split(' ')
+        .map((str) => validURL(str) ? str : false)
+        .filter(item => item !== false)[0];
+    const renderLinkPreview = firstLink ? <ReactTinyLink
+        cardSize='small'
+        showGraphic={true}
+        maxLine={2}
+        minLine={1}
+        url={formatUrl(firstLink)}
+    /> : null;
+
     return(
         <div className={classNames(styles.MessageItem, 'pb-3')}>
             <div className={styles.MetaBox}>
-                <span className="font-weight-bold mr-2">{user.name}</span>
+                <span className='font-weight-bold mr-2'>{user.name}</span>
                 <span className={styles.Gray}>{!wasEdited ? created : `${updated} Edited`}</span>
                 <span>{isOwner ? <EditBox message={message} /> : null}</span></div>
             <div className={(message.userId === '100' || message.isDeleted) ? styles.Gray : ''}>
                 {editMessage.id === message.id ? <EditInput /> : renderMessage}
             </div>
+            {renderLinkPreview}
         </div>
     )
 }
---
+++ src/utils/UrlHelper.js
@@ -0,0 +1,19 @@
+const validURL = (str) => {
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
+
+const formatUrl = (str) => {
+    const hasProtocol = !!new RegExp('^(https?:\\/\\/)').test(str);
+    return !hasProtocol ? 'http://'+str : str
+}
+
+export {
+    validURL,
+    formatUrl,
+}
